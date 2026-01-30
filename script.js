598183bb70f23c295e0869d83f73e3c3fd7df693
luiiz2 <luizarturpombo13@gmail.com>
2026-01-30 12:11:18 -0300

    logica do  jogo da velha

--- script.js
+++ script.js
@@ -11,9 +11,45 @@ function clicknacasa(event) {
         return;
     }
     casaClicada.innerText = jogadorAtual;
+    if (checarVencedor()) {
+        setTimeout(() => {
+            alert(`Jogador ${jogadorAtual} venceu!`);
+            reiniciarJogo();
+        }, 100);
+        return;
+    }
     if (jogadorAtual === 'X') {
         jogadorAtual = 'O';
     } else {
         jogadorAtual = 'X';
     }
 }
+
+function reiniciarJogo() {
+    casas.forEach(casa => {
+        casa.innerText = '';
+    });
+    jogadorAtual = 'X'; // O X começa sempre
+}
+
+function checarVencedor() {
+    const combinacoesVencedoras = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let combinacao of combinacoesVencedoras) {
+        const [a, b, c] = combinacao;
+        if (casas[a].innerText !== '' &&
+            casas[a].innerText === casas[b].innerText &&
+            casas[a].innerText === casas[c].innerText) {
+            return true;
+            }
+    }
+    return false;
+        }
